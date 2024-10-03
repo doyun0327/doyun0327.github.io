d0024b5ce4a93209d7753cda1ed85826364ed977
leedoyun <idoyun781@gmail.com>
2024-10-03 16:58:31 +0900

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+         <div className="App">
+      <header className="App-header">
+        <h1>안녕하세요!</h1>
+        <p>저는 [이또찌맘]입니다.</p>
+        <p>여기에서 저에 대해 소개할 수 있습니다.</p>
+      </header>
+    </div>
+    </div>
+  );
+}
+
+export default App;
