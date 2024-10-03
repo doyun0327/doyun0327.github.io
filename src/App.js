884fe3edab90c989258eb3f64b9578426c97a23c
leedoyun <idoyun781@gmail.com>
2024-10-03 20:21:26 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
     <div className="App">
          <div className="App">
       <header className="App-header">
-        <h1>안녕하세요!</h1>
-        <p>저는 [이또찌맘]입니다.</p>
-        <p>여기에서 저에 대해 소개할 수 있습니다.</p>
+        <h1>다개미의 취업을 추카합니댜~~</h1>
+        {/* <p>저는 [이또찌맘]입니다.</p>
+        <p>여기에서 저에 대해 소개할 수 있습니다. hihi</p> */}
       </header>
     </div>
     </div>
