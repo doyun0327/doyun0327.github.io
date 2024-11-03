644a57e2fa2f9760146dd2e318695a2363a9927c
leedoyun <idoyun781@gmail.com>
2024-11-03 15:41:19 +0900

    myintroserver제거

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
       <h1>안녕하세요!</h1>
         <p>저는 [이또찌맘]입니다ㅇ.ㅇ</p>
-        <p>여기에서 저에 대해 소개하겠어요</p>
+        <p>여기에서 저에 대해 소개하겠어요zz</p>
       </header>
     </div>
   );
