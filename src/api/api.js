bd20180f0467ac40052ff7238e29bdd3fac15be2
leedoyun <idoyun781@gmail.com>
2024-12-21 20:27:12 +0900

    env파일 수정/운영에서 서버 접속 가능하도록록

--- src/api/api.js
+++ src/api/api.js
@@ -4,10 +4,14 @@ import {
   isAccessTokenExpired,
   refreshAccessToken,
 } from "../utils/auth";
+//require("dotenv").config({ path: "../.env" });
+
 
 //Axios 인스턴스 생성
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: process.env.NODE_ENV === 'production' 
+  ? process.env.REACT_APP_API_BASE_URL_PROD 
+  : process.env.REACT_APP_API_BASE_URL,
   headers: { "Content-Type": "application/json" },
 });
 
