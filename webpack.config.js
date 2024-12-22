90ba80a4c18bcfad6d8cf29eb9b3330c6298ff86
leedoyun <idoyun781@gmail.com>
2024-12-22 18:28:14 +0900

    e

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
+console.log('webpack.config.js is loaded');
+
 module.exports = {
   devServer: {
-    port: 3000,  // 개발 서버 포트, 클라이언트 앱 포트와 일치
+    port: 3000,
     proxy: {
       '/': {
-        target: 'http://localhost:4000',  // 백엔드 서버의 포트
-        changeOrigin: true,               // 요청의 Origin을 타겟 서버로 변경
-        secure: false,                    // HTTPS를 사용할 경우 true로 설정
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
       }
+    },
+    before: (app, server, compiler) => {
+      console.log('Webpack dev server is starting...');
+      console.log('DevServer Configuration:', module.exports.devServer);
     }
   }
 };
+
+
 
 // 로그 찍기
 console.log('Webpack devServer configuration:', module.exports.devServer);
